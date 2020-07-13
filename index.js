de1e2d239f932dd5f3c470e64d711ff6a1abfc0a
Amanda Manço <amand113@hotmail.com>
2020-07-12 23:36:47 -0300

    Readme

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const mdLinks = (path) => {
     return new Promise((resolve, reject) => {
       fs.readdir(path, function (err, files) {
         const filesMd = files.filter(function (file) {
-          return extname(file) === ".md";
+          return extname(file) === '.md';
         });
         if (filesMd.length === 0) {
           return reject(`.md files not found in this directory`);
@@ -38,16 +38,14 @@ const mdLinks = (path) => {
           filesMd.map(function (files) {
             const replaceDirectory = join(path, files);
             const directoryPath = replaceDirectory.replace("\\", "/");
-            const teste = directoryPath.replace('./', '');
-            console.log(teste)
-            showFiles(resolve, reject, teste);
+            showFiles(resolve, reject, directoryPath);
           });
         };
       });
     });
   } else {
     return new Promise((resolve, reject) => {
-      if (extname(path) !== ".md") {
+      if (extname(path) !== '.md') {
         return reject(`.md file not found`);
       } else {
         showFiles(resolve, reject, path);
